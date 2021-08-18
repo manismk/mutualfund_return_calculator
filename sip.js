8e7e33d154f0d7baadee2fc25779476055da67e3
manismk <manismkmech@gmail.com>
2021-08-18 16:23:33 +0530

    Update: changed to green theme and added lumpsum also

--- sip.js
+++ sip.js
@@ -7,12 +7,14 @@ var years = document.querySelector("#years");
 var invested = document.querySelector("#invested");
 var expected = document.querySelector("#expected");
 var total = document.querySelector("#total");
+var investType = document.querySelector("#investType");
+var investLabel = document.querySelector("#investLabel");
 var chart;
 var initialLoad = true;
 
 var xValues = ["Invested", "Expected Returns"];
 var yValues = [1, 1];
-var barColors = ["#b91d47", "#00aba9"];
+var barColors = ["#10B981", "#047857"];
 
 sliderPurchaseInput.oninput = function () {
   updateslider(sliderPurchaseInput, purchase);
@@ -24,36 +26,60 @@ sliderYearsInput.oninput = function () {
   updateslider(sliderYearsInput, years);
 };
 window.onload = function () {
-  updateslider();
+  sliderPurchaseInput.value = 15000;
+  sliderRateInput.value = 15;
+  sliderYearsInput.value = 15;
+  updateslider(sliderPurchaseInput, purchase);
+  updateslider(sliderRateInput, rate);
+  updateslider(sliderYearsInput, years);
 };
 
+for (var i = 0, max = investType.length; i < max; i++) {
+  investType[i].onclick = function () {
+    updateslider();
+    if (this.value === "sip") {
+      investLabel.innerHTML = "Monthly Invesment";
+    } else {
+      investLabel.innerHTML = "Onetime Invesment";
+    }
+  };
+}
 function updateslider(slider, spanName) {
   if (slider) {
-    spanName.innerText = slider.value;
+    spanName.innerText = numberConvertToIndian(slider.value);
     var value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
-    var color = `linear-gradient(to right, red 0%, red ${value}%, grey ${value}%, grey 100%)`;
+    var color = `linear-gradient(to right, #047857 0%, #047857 ${value}%, #6EE7B7 ${value}%, #6EE7B7 100%)`;
     slider.style.background = color;
   }
+  var type = investType.elements[`invesmentType`].value;
 
   var purchaseValue = sliderPurchaseInput.value;
   var rateValue = sliderRateInput.value;
   var yearsValue = sliderYearsInput.value;
   var months = yearsValue * 12;
   var monthRate = rateValue / (100 * 12);
-  var investedAmount = purchaseValue * months;
-
-  //FV = P [ (1+i)^n-1 ] * (1+i)/i
   var sip =
     purchaseValue *
     (Math.pow(1 + monthRate, months) - 1) *
     ((1 + monthRate) / monthRate);
-  sip = Math.round(sip);
+  var lumpsum = Math.round(
+    Math.pow(1 + rateValue / 100, yearsValue) * purchaseValue
+  );
+  var totalAmount = 0;
+  if (type === "sip") {
+    var investedAmount = purchaseValue * months;
+    sip = Math.round(sip);
+    totalAmount = sip;
+  } else {
+    var investedAmount = purchaseValue;
+    totalAmount = lumpsum;
+  }
 
-  var ExpectedAmount = sip - investedAmount;
-
+  //FV = P [ (1+i)^n-1 ] * (1+i)/i
+  var ExpectedAmount = totalAmount - investedAmount;
   invested.innerText = numberConvertToIndian(investedAmount);
   expected.innerText = numberConvertToIndian(ExpectedAmount);
-  total.innerText = numberConvertToIndian(sip);
+  total.innerText = numberConvertToIndian(totalAmount);
 
   if (!initialLoad) {
     chart.destroy();
